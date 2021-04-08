5e113e77af97b88e554ac3cb875ed8150e7856df
Emanuela Quizini <manu.biq@gmail.com>
2021-04-07 23:16:43 -0300

    Vitoria na vertical

--- teste.js
+++ teste.js
@@ -8,3 +8,18 @@ let touchColumn = ev.currentTarget.id
     vai.appendChild(filho);
     console.log(ev)
     console.log(touchColumn);
+
+    if(arr[0][5] === "B"){
+        console.log("miau")
+       
+    } if (arr[1][5] === "R") {
+        console.log("Katiau")
+    }
+
+     // for(let i = 0; i < 7; i++){
+    //     // passar pelas coluns na cel baseada no ind;
+    //     if(arr[i][ind] === "B"){
+    //         console.log(arr[i][ind]);
+    //     }
+        
+    // }
